ca1646d53b70079391f799e316af4213e1d34b8f
Ryan Haughton <rahaughton@g.hmc.edu>
2022-01-21 09:47:36 -0800

    Fixed nefarious bug that was preventing postgres usage

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const devConfig = new Pool({
+const devConfig = {
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     host: process.env.PG_HOST,
-    port: process.env.PORT,
+    port: process.env.PG_PORT,
     database: process.env.PG_DATABASE
-})
+};
 
 const pool = new Pool(devConfig);
 
